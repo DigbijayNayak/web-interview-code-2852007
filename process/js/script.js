1b708ec49b4c9db63c1d806121c98a03049deb35
Digbijay Nayak <66635084+DigbijayNayak@users.noreply.github.com>
2025-01-12 15:29:52 +0000

    are you comfortable using jQuery?

--- process/js/script.js
+++ process/js/script.js
@@ -15,3 +15,18 @@ highlight.forEach(function (item) {
         item.style.backgroundColor = 'blue';
     }, false);
 }, this);
+
+$(function() {
+    $('.load-info').on('click', function() {
+        $.ajax({
+            method: 'GET',
+            url: 'js/data_artists.json',
+            dataType: 'json'
+        }).done(function(data) {
+            $.each(data.artists, function(key, val){
+                $('.info-artists')
+                    .append('<li>' + val.name + '</li>')
+            }); //each
+        }); //ajax done
+    }); // click event
+}); // jQuery Ready
