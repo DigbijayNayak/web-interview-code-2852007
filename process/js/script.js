2031262f31cb6200848df754ea10e93d56926dbf
Digbijay Nayak <66635084+DigbijayNayak@users.noreply.github.com>
2025-01-13 10:38:18 +0000

    how do you use a constructor to create instances?

--- process/js/script.js
+++ process/js/script.js
@@ -129,3 +129,27 @@ function makeBoxes(howMany) {
 }
 
 makeBoxes(20)
+
+// constructor
+
+function Hamburger(nodeName) {
+    var myNode = document.querySelector(nodeName + ' .hamburger');
+  
+    return {
+      activate: function() {
+        myNode.addEventListener('click', function(e) {
+        myNode.parentNode.querySelector('.navbar').classList.toggle('hidden');
+        }, false);
+      }, // activate
+      hide: function() {
+        myNode.parentNode.querySelector('.navbar').classList.add('hidden');
+      } // hide
+    } //return
+  } //Hamburger
+  
+  var topMenu = new Hamburger('#topMenu');
+  topMenu.activate();
+  
+  var bottomMenu = new Hamburger('#bottomMenu');
+  bottomMenu.activate();
+  bottomMenu.hide();
