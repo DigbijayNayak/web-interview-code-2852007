380baa15f9f9213ca36b4c7e5250af30016fd9cd
Digbijay Nayak <66635084+DigbijayNayak@users.noreply.github.com>
2025-02-01 13:57:26 +0000

    what is the relationship between promises and callbacks?

--- process/js/script.js
+++ process/js/script.js
@@ -239,3 +239,38 @@ for(var i = 0; i < tips.length; i++) {
     e.target.querySelector('.tooltips').classList.toggle('active');
   }, false);
 }
+
+// Relationship btw promises and callbacks
+
+var artists, calendar;
+
+function loadData(url) {
+  return new Promise(function (resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function () {
+      if (req.status == 200) {
+        console.log('getting ' + url);
+        resolve(JSON.parse(req.response))
+      } else {
+        reject(Error('Problem loading data'))
+      }
+    } // onload callback
+    req.send();
+  });
+}
+
+calendar = loadData('js/data_schedule.json');
+artists = loadData('js/data_artists.json');
+
+document.querySelector('.loads-info')
+  .addEventListener('click', function () {
+    calendar.then(function (data) {
+      console.log(data);
+      return artists;
+    }, function (error) {
+      console.log('Problem', error);
+    }).then(function (otherData) {
+      console.log(otherData);
+    });
+  });
